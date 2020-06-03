25ec455fb753c297495bfd530829cd5b5a5d75c1
Khamari Thompson <61212885+Khamari13@users.noreply.github.com>
2020-06-03 17:07:15 -0400

    added notes and api route

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = reuire("express");
 const app = express();
 const path = require('path');
-
+const fs = require("fs");
 const PORT = process.env.PORT || 8080;
+const savedNotes = require("./db/db.json");
 
 app.use(express.urlencode({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'))
 
-const fs = require("fs");
+//html route
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
 
+//api routes
+app.get('/api/notes', function (req, res) {
+    return res.json()
+})
 
 fs.writefile(
     "db.json",
