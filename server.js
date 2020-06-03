ea5639a16c9384cd46b4bc0e363e1a70c4268e72
Khamari Thompson <61212885+Khamari13@users.noreply.github.com>
2020-06-03 16:47:15 -0400

    updated index.js and moved json packages

--- server.js
+++ server.js
@@ -7,5 +7,28 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencode({ extended: true }));
 app.use(express.json());
 
+const fs = require("fs");
+
+
+fs.writefile(
+    "db.json",
+    JSON.stringify(notes), 
+    (err) => {
+        if(err) {
+            throw err;
+        }
+    }
+)
+
+fs.readfile(
+    "db.json",
+    (err, results) => {
+        if(err) {
+            throw err;
+        }else {
+            notes = JSON.parse
+        }
+    }
+)
 
 
