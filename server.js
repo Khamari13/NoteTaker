5f4dd93f35cb6e0e96bc044129dc69a46b9ecf2a
Khamari Thompson <61212885+Khamari13@users.noreply.github.com>
2020-06-03 16:38:05 -0400

    added json packages

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
+const express = reuire("express");
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencode({ extended: true }));
+app.use(express.json());
+
 
 
