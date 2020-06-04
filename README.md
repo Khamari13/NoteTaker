975118cc941cd2fdb3a071ab0036364c38d26643
Khamari Thompson <61212885+Khamari13@users.noreply.github.com>
2020-06-03 22:43:05 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# NoteTaker
+## NoteTaker
+
+## In the code, we create an app that allows the user to manually take notes on his/her device. To set this up we used GET, POST, and DELETE routes to combine the pages. The main modules used in this app are express.
+
+## https://safe-temple-97467.herokuapp.com/notes
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const fs = require("fs");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3500;
 const savedNotes = require("./db/db.json");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,34 +11,29 @@ app.use(express.static('public'))
 app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 })
-//api routes
+// api routes
 app.get('/api/notes', function (req, res) {
     return res.json(savedNotes)
 })
 app.delete('/api/notes/:id', function (req, res) {
     let uniqId = parseInt(req.params.id)
-    let uniqArr = savedNotes.filter(function(newNote){
-        newNote.id !== uniqId
-    })
-    fs.writeFile("./db/db.json", JSON.stringify(uniqArr), err =>{
+    let uniqArr = savedNotes.filter(newNote => newNote.id !== uniqId)
+    fs.writeFile("./db/db.json", JSON.stringify(uniqArr), function(err) {
         if (err) console.log(err)
-        res.send(savedNotes)
+        res.end()
       })
 })
 //api post request
 app.post('/api/notes', function (req, res) {
-    let clientNote = req.body;
+    const clientNote = req.body;
     clientNote.id = savedNotes.length +1
     savedNotes.push(clientNote)
-    fs.writeFile("./db/db.json", JSON.stringify(savedNotes), err =>{
+    fs.writeFile("./db/db.json", JSON.stringify(savedNotes), function(err) {
         if (err) console.log(err)
-        res.send(savedNotes)
+        res.end()
       })
 })
-//Port Listener
+//port listener
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-  });
-
-
-
+    console.log("App listening on PORT:" + PORT);
+});
