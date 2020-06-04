d91eca545eb273d973b4d0b7cfe074d0374a3ec9
Khamari Thompson <61212885+Khamari13@users.noreply.github.com>
2020-06-03 20:36:23 -0400

    removed spaces

--- server.js
+++ server.js
@@ -2,24 +2,19 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const fs = require("fs");
-
 const PORT = process.env.PORT || 8080;
 const savedNotes = require("./db/db.json");
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
-
 //html route
 app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 })
-
 //api routes
 app.get('/api/notes', function (req, res) {
     return res.json(savedNotes)
 })
-
 app.delete('/api/notes/:id', function (req, res) {
     let uniqId = parseInt(req.params.id)
     let uniqArr = savedNotes.filter(function(newNote){
@@ -30,7 +25,6 @@ app.delete('/api/notes/:id', function (req, res) {
         res.send(savedNotes)
       })
 })
-
 //api post request
 app.post('/api/notes', function (req, res) {
     let clientNote = req.body;
@@ -41,7 +35,6 @@ app.post('/api/notes', function (req, res) {
         res.send(savedNotes)
       })
 })
-
 //Port Listener
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
